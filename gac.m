79e97485f4d5286f41c0d6346b39907c6140ae94
Brandon <bschloma@uoregon.edu>
2018-05-23 12:04:44 -0700

    cleaned up gac.m and included input arguments for aggregation and fragmentation exponents.

--- gac.m
+++ gac.m
@@ -2,13 +2,13 @@
 % Growth and Aggregation of Clusters model
 %
 
-function [V_arr,total_pop_arr] = gac(lr,la,lc,ls,dt,Tmax,n0)
+function [V_arr,total_pop_arr] = gac(lr,la,lc,ls,dt,Tmax,n0,nu_A,nu_F)
 
 %% default values for input parameters
 
 % rate of cluster growth
 if ~exist('lr','var')||isempty(lr)
-    lr = 1.4;
+    lr = 1;
 end
 
 % rate of cluster aggregation
@@ -18,7 +18,7 @@ end
 
 % rate of cluster collapse
 if ~exist('lc','var')||isempty(lc)
-    lc = .1;
+    lc = .005;
 end
 
 % rate of cluster sprouting
@@ -41,15 +41,30 @@ if ~exist('n0','var')||isempty(n0)
     n0 = 10;
 end
 
+% aggregation exponent
+if ~exist('nu_A','var')||isempty(nu_A)
+    nu_A = 2/3;
+end
+
+% fragmentation exponent
+if ~exist('nu_F','var')||isempty(nu_F)
+    nu_F = 2/3;
+end
 % shuffle random number generator
 rng('shuffle');
-
 
 
 %% intialize some arrays
 
 % main object of the simulation:  array of cluster volumes
-V_arr = ones(1,n0);
+if numel(n0)==1
+    V_arr = ones(1,n0);
+elseif size(n0,1) > 1
+    disp('gac error:  initial cluster size array n0 must have dim 1xM');
+    return
+else
+    V_arr = n0;
+end
 
 % time vector
 tvec = 0:dt:Tmax;
@@ -59,40 +74,18 @@ numsteps = numel(tvec);
 
 % array for keeping track of total population size
 total_pop_arr = zeros(1,numsteps);
-total_pop_arr(1) = n0;
+total_pop_arr(1) = sum(n0);
 
 % pregenerate lots of random numbers for speed.
-number_of_random_numbers_to_pre_generate = 1000.*numsteps;
+number_of_random_numbers_to_pre_generate = round(1000.*numsteps);
 lots_of_random_numbers = rand(1,number_of_random_numbers_to_pre_generate);
 
 % counter for how many of the pre-generated random numbers have been used.
 n = 0;
 
 %% fixed parameters
-% method for regulating growth of clusters.
-%	"global' =  cluster growth is regulated by the total
-%               population size, max_tot_pop.  
-%   'local' =   cluster growth is regulated by local
-%               constraints, i.e. space or local nutrient depletion, via a maximum
-%               volume for a cluster, Vmax.
-%
-cluster_growth_regulation_method = 'global';
-
 % carrying capacity of whole system.  regulates cluster growth in 'global' mode.  
 max_total_pop = 10^(4);                       
-
-% maximum volume of individual clump.  regulates cluster growth in 'local' model, and
-% also sets scale for how probability of collapse depends on cluster size.
-Vmax = max_total_pop;%1e4;
-
-% NOT USED YET.exponent for determining aggregation probability.  Pa ~ V^theta.
-% theta = 0:    all clusters have equal prob. of aggregating.
-% theta = 2/3:  Pa ~ surface area of cluster.
-% theta = 1:    Pa ~ volume of cluster.
-theta = 2/3;
-
-% logical for stochastic vs deterministic growth
-l_stochastic_growth = false;
 
 % logical for printing progress to screen
 l_print_progress = false;
@@ -107,65 +100,12 @@ for s = 2:numsteps
     
     %% growth
     
-    switch l_stochastic_growth
-        case true
-            
-            % collect random numbers.  If we've run out of pregenerated ones,
-            % generate some new ones.
-            if n + numel(V_arr) < number_of_random_numbers_to_pre_generate
-                random_numbers = lots_of_random_numbers(n+1:n+numel(V_arr));
-                n = n + numel(V_arr);
-            else
-                random_numbers = rand(1,numel(V_arr));
-            end
-            
-            % compute probability of clusters growing in this timestep.  there are
-            % currently two methods.  'global' = cluster growth is regulated by the total
-            % population size, max_tot_pop.  'local' = cluster growth is regulated by local
-            % constraints, i.e. space or local nutrient depletion, via a maximum
-            % volume for a cluster, Vmax.
-            switch cluster_growth_regulation_method
-                
-                case 'global'
-                    
-                    Pr = lr.*(1-sum(V_arr)./max_total_pop).*dt.*ones(size(V_arr));
-                    
-                case 'local'
-                    
-                    Pr = lr.*(1-V_arr./Vmax).*dt;
-                    
-                case 'both'
-                    
-                    Pr = lr.*(1-V_arr./Vmax).*dt.*(1-sum(V_arr)./max_total_pop);
-                    
-                    
-            end
-            
-            % grow clusters in one vectorized step
-            V_arr(random_numbers <= Pr) = 2.*V_arr(random_numbers <= Pr);
+    % grow all clusters in one deterministic vectorized step
+    V_arr = V_arr + dt.*lr.*V_arr.*(1-sum(V_arr)./max_total_pop);
     
-    case false
-            
-        switch cluster_growth_regulation_method
-            
-            case 'global'
-                % grow clusters in one vectorized step
-                V_arr = V_arr + dt.*lr./log2(exp(1)).*V_arr.*(1-sum(V_arr)./max_total_pop);
-            
-            case 'local'
+    % if clusters die out, remove them
+    V_arr = V_arr(V_arr>=1);
                 
-                % grow clusters in one vectorized step
-                V_arr = V_arr + dt.*lr./log2(exp(1)).*V_arr.*(1-V_arr./Vmax);
-                
-            case 'both'
-                
-                % grow clusters in one vectorized step
-                V_arr = V_arr + dt.*lr./log2(exp(1)).*V_arr.*(1-sum(V_arr)./max_total_pop).*(1-V_arr./Vmax);
-        end
-        
-        V_arr = V_arr(V_arr>=1);
-                
-    end
     %% collapse 
     
     % collect random numbers.  If we've run out of pregenerated ones,
@@ -178,16 +118,12 @@ for s = 2:numsteps
     end
     
     % compute probability of collapse as a function of cluster volume.
-    % here, the relationship is linear and scaled by Vmax.  
-    %Pc = lc.*V_arr./Vmax.*dt;
-    
-    % here, the relationship is linear in linear length dimension and scaled by Vmax.
-    Pc = lc.*(V_arr./Vmax).^(1/3).*dt;
+    % here, the relationship is linear in linear length dimension.
+    Pc = lc.*(V_arr).^(1/3).*dt;
 
     % remove clusters that have collapsed from the cluster array.
     V_arr = V_arr(random_numbers >= Pc);
-    
-    
+        
     %% aggregation
     
     % collect random numbers.  If we've run out of pregenerated ones,
@@ -199,30 +135,34 @@ for s = 2:numsteps
         random_numbers = rand(1,numel(V_arr));
     end
     
-    % compute the probability of aggregation in this timestep.  
-    Pa = la.*dt;
+    % compute the probability of aggregation in this timestep. 
+    % made product kernel 5/1/18
+    Pa = la.*dt.*V_arr.^nu_A;
     
     % find ids of clusters that will aggregate in this timestep
     agg_ids = find(random_numbers < Pa);
     
     % loop over aggregates
     for c = 1:numel(agg_ids)
+              
+        % choose a cluster at random 
+        %random_cluster_id = randi([1,numel(V_arr)]);
+        % made product kernel 5/1/18: pick a cluster based on its size.
+        % construct a discrete probability distribution for clusters by
+        % normalizing the cluster size array and inverse project uniform
+        % random numbers via the cumulative distribution function.
+
+        rn = rand();
+        random_cluster_id = find(rn <= cumsum(V_arr.^nu_A./sum(V_arr.^nu_A)),1);
         
-        % agg_ids can change size, need this check
-        if c > numel(agg_ids)
-            break
-        end
-        
-        % choose a cluster at random
-        random_cluster_id = randi([1,numel(V_arr)]);
-        
-        %if sum(ismember([agg_ids,0],random_cluster_id)) == 0
         if random_cluster_id ~= agg_ids(c)
             
             % add cluster sizes
             V_arr(agg_ids(c)) = V_arr(agg_ids(c)) + V_arr(random_cluster_id);
             
-            % mark randomly chosen cluster for removal with a 0
+            % mark randomly chosen cluster for removal with a 0.  Using the
+            % product kernel code above, this guarantees that this cluster
+            % will not be chosen again.
             V_arr(random_cluster_id) = 0;
             
             % if the randomly chosen cluster was part of the original
@@ -252,7 +192,7 @@ for s = 2:numsteps
  
     % compute probability of sprouting.  require clusters to be of at least
     % size 2 to sprout.
-    Ps = ls.*dt.*(V_arr >= 2).*(V_arr).^(2/3);
+    Ps = ls.*dt.*(V_arr >= 2).*(V_arr).^(nu_F);
     
     % find ids of clusters that sprouted
     sprout_ids = find(random_numbers < Ps);
